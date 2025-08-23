32e76ad0f87e12a3b404478392730cc35b16d629
amazia <amaziakristanto03@gmail.com>
2025-08-24 01:39:16 +0700

    update

--- js/script.js
+++ js/script.js
@@ -591,44 +591,44 @@ const App = (() => {
       return;
     }
     tsParticles.load("particles-js", {
-      particles: {
-        number: { value: 50, density: { enable: true, value_area: 800 } },
-        color: { value: "#ffffff" },
-        shape: { type: "circle" },
-        opacity: {
-          value: 0.4,
-          random: true,
-          anim: { enable: true, speed: 1, opacity_min: 0.1, sync: false },
-        },
-        size: { value: 2, random: true },
-        line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#ffffff",
-          opacity: 0.2,
-          width: 1,
-        },
-        move: {
-          enable: true,
-          speed: 1,
-          direction: "none",
-          random: false,
-          straight: false,
-          out_mode: "out",
-        },
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: { enable: true, mode: "repulse" },
-          onclick: { enable: true, mode: "push" },
-          resize: true,
-        },
-        modes: {
-          repulse: { distance: 100, duration: 0.4 },
-          push: { particles_nb: 4 },
-        },
-      },
+      // particles: {
+      //   number: { value: 50, density: { enable: true, value_area: 800 } },
+      //   color: { value: "#ffffff" },
+      //   shape: { type: "circle" },
+      //   opacity: {
+      //     value: 0.4,
+      //     random: true,
+      //     anim: { enable: true, speed: 1, opacity_min: 0.1, sync: false },
+      //   },
+      //   size: { value: 2, random: true },
+      //   line_linked: {
+      //     enable: true,
+      //     distance: 150,
+      //     color: "#ffffff",
+      //     opacity: 0.2,
+      //     width: 1,
+      //   },
+      //   move: {
+      //     enable: true,
+      //     speed: 1,
+      //     direction: "none",
+      //     random: false,
+      //     straight: false,
+      //     out_mode: "out",
+      //   },
+      // },
+      // interactivity: {
+      //   detect_on: "canvas",
+      //   events: {
+      //     onhover: { enable: true, mode: "repulse" },
+      //     onclick: { enable: true, mode: "push" },
+      //     resize: true,
+      //   },
+      //   modes: {
+      //     repulse: { distance: 100, duration: 0.4 },
+      //     push: { particles_nb: 4 },
+      //   },
+      // },
       retina_detect: true,
     });
   }
